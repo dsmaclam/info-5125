aaf26794cc656c16372f533b2332f08653045d8e
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-02-02 12:13:07 -0500

    added prototype starter code for section 02

---
+++ section_02/week_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4_Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User alice = new User();
+            alice.FirstName = "Alice";
+            alice.LastName = "Smith";
+            alice.Rank = "Employee";
+
+            User bob = alice.Clone() as User;
+            bob.FirstName = "Bob";
+
+            alice.Print();
+            bob.Print();
+        }
+    }
+}
