e484863372d3ed1244689421b08acad36fd40bd8
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-02-10 10:26:52 -0500

    template method starting point

---
+++ section_01/week_05/5_TemplateMethod2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5_TemplateMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //all worker have the same daily routine
+            //that will be implemented by using a template method
+
+            var workers = new List<Worker>
+            {
+                //new Firefighter(), 
+                //new Teacher(),
+                //new Police_Officer(),
+                //new SWATOfficer()
+            };
+
+            foreach (var worker in workers)
+            {
+                Console.WriteLine(worker.GetWorkerType() + ":");
+
+                //call the template method on the worker
+                worker.DailyRoutine();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
