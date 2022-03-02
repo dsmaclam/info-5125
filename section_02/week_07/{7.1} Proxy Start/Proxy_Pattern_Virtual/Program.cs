aecfe71f33356fa9b1fe6d1b05a7274caf2f2fac
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-02 12:50:50 -0500

    added starter code week 7 sec 2

---
+++ section_02/week_07/{7.1} Proxy Start/Proxy_Pattern_Virtual/Program.cs
@@ -0,0 +1,16 @@
+﻿//this program shows the proxy pattern to handle caching data and lazy loading it
+using System;
+
+namespace Proxy_Pattern_Virtual
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var proxySubject = new ProxySubject();
+
+            proxySubject.Request();
+            proxySubject.Request();
+        }
+    }
+}
