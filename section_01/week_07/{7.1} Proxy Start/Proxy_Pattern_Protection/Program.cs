df6c2e62b0cb6145eb794cb833c27a20d001d463
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-03 10:42:28 -0500

    added starter files sec 01

---
+++ section_01/week_07/{7.1} Proxy Start/Proxy_Pattern_Protection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Proxy_Pattern_Protection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lets look at the behaviour for the real request
+            RealRequest rr = new RealRequest();
+            Console.WriteLine("RealRequest:");
+            Console.WriteLine("\t" + rr.Request("testing.com"));
+            Console.WriteLine("\t" + rr.Request("admin.testing.com"));
+
+            //lets use our proxy now to control access to the real request
+            //User user = new User("Bob", User.Roles.Customer);
+            //ProxyRequest pr = new ProxyRequest(rr, user);
+            //Console.WriteLine("ProxyRequest:");
+            //Console.WriteLine("\t" + pr.Request("testing.com"));
+            //Console.WriteLine("\t" + pr.Request("admin.testing.com"));
+        }
+    }
+}
