ffeaf3b4a0c0cd4c04b2460e21e2f666fe022a21
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-09 12:26:38 -0500

    added section 02 command starting code

---
+++ section_02/week_08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //receiver here would be some context/object you want to do something to (e.g. Document)
+            Document receiver = new Document(); 
+
+            //command that represents an action
+            //Command c = new OpenDocumentCommand(receiver, "pdf"); 
+
+            //use an action to trigger the command 
+            //Button b = new Button("Open Document", c);
+            //b.Click();
+        }
+    }
+}
