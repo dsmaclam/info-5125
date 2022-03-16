2a849e8b350436a73b23baf42310b54be3454c81
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-16 12:22:03 -0400

    added CoR start code sec 02

---
+++ section_02/week_09/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _8_CoR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application a = new Application();
+            //Window w = new Window(a);
+            //DialogBox db = new DialogBox(w);
+            //PrintButton pb = new PrintButton(db);
+
+            //pb.HandleRequest();
+        }
+    }
+}
