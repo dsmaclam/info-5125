0803262580f032549017d79eb13ee3975daec7e0
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-23 12:22:10 -0400

    added sec02 visitor starter code

---
+++ section_02/week_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _9_Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Element componentA = new Circle();
+            Element componentB = new Circle();
+            Element componentC = new Text();
+
+            // Visitor saver = new PNGVisitor();
+            Visitor saver = new JPEGVisitor();
+            componentA.Accept(saver);
+            componentB.Accept(saver);
+            componentC.Accept(saver);
+        }
+    }
+}
