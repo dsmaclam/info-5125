520574e02cce1c5a0ce2d3036179dc702a63b952
Daniel Maclam <d_maclam@fanshaweonline.ca>
2022-03-30 12:44:25 -0400

    sec02 mvc starter code

---
+++ section_02/week_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _11_MVC
+{
+    class Program
+    {
+        static Student LoadFromDb()
+        {   
+            Student student = new Student();
+
+            student.setName("Bob");
+            student.setId("1");
+
+            return student;
+        }
+
+        static void Main(string[] args)
+        {
+            Student model = LoadFromDb();
+            //View view = new View();
+            //Controller controller = new Controller(model, view);
+
+            //controller.UpdateView();
+            
+            //model.setName("Sam");
+            //model.setId("123");
+            //controller.UpdateView();
+        }
+        
+    }
+}
